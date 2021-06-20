147ff199d9d2b6ab1b03e8e43b7ec35f6cefdca9
Aarnoudse <83244265+Aarnoudse@users.noreply.github.com>
2021-06-20 18:34:22 +0200

    Add files via upload

---
+++ WINC_023_Selecteer_Elementen/script.js
@@ -0,0 +1,9 @@
+//document.body.innerHTML = "Dit is een nieuwe paragraaf"
+
+//let firstsection = document.getElementById('first-section');
+
+//console.log(firstsection);
+
+//let par = document.getElementsByClassName('paragraph');
+
+//console.log(par);
